2cd4126b4d46bca91eec059cdd0a8b2289b3c8e7
Papillon6814 <so1rou.k@gmail.com>
2020-09-25 23:08:24 +0900

    プロジェクト作成

---
+++ shpotify/shpotify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  shpotify
+//
+//  Created by 久野聡一郎 on 2020/09/25.
+//  Copyright © 2020 株式会社Papillon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ shpotify/shpotifyTests/shpotifyTests.swift
@@ -0,0 +1,34 @@
+//
+//  shpotifyTests.swift
+//  shpotifyTests
+//
+//  Created by 久野聡一郎 on 2020/09/25.
+//  Copyright © 2020 株式会社Papillon. All rights reserved.
+//
+
+import XCTest
+@testable import shpotify
+
+class shpotifyTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
